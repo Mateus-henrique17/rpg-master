e278901065891ef8440d113cfe5a40e82c3c2243
Mateus-henrique17 <1205mateus@gmail.com>
2026-07-25 11:42:13 -0300

    ajustando as confg do deploy

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,6 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-import { SYSTEM_INSTRUCTION } from "../config/rpgConfig.js";
-
+import { SYSTEM_INSTRUCTION } from "../config/rpgconfig.js";
 const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
 
 const MODEL_NAME = "gemini-2.5-flash";
